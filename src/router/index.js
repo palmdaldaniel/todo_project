6f1ed55be67ac089326f93ed9b0d7f9415de6cef
Daniel Palmdal <daniel.palmdal@gmail.com>
2021-01-27 09:18:35 +0100

    implement router view for todo

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Todo from '../views/todoSite.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +16,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
-  }
+  }, 
+  {
+  path: '/todoSite',
+  name: 'Todo',
+  component: Todo
+}
 ]
 
 const router = new VueRouter({
---
+++ src/views/todoSite.vue
@@ -0,0 +1,17 @@
+<template>
+
+<div class="todoSite">
+    <h1> Let there be todos ! </h1>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
